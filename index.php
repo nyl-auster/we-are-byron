79f9748139e847466de622350b543e77696b05e3
yann <yann.boisselier@gmail.com>
2015-05-01 19:43:16 +0200

    up

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Byron</title>
+  <meta name="description" content="Byron est l'enfant terrible de Captain Beefheart et The Streets, le batard révolté de Tom Waits
+            et The Beastie Boys. Avec un hip-hop cinématographique qui emprunte au blues, au gospel, au folk et au punk, et des textes en anglais incisifs, ils vous entraînent dans les méandres du désespoir amoureux sur un air enjoué de cabaret glauque." />
+  <meta name="keywords" content="musique, groupe, byron, band, music">
+  <link rel="stylesheet" href="css/app.css" />
+  <script src="bower_components/modernizr/modernizr.js"></script>
+</head>
+
+<body>
+
+<div id="fb-root"></div>
+
+<script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="row">
+  <div class="small-12 columns">
+    <h1 class="text-center" id="byron">BYRON</h1>
+  </div>
+</div>
+
+<div class="row content">
+  <div class="small-12 medium-12 columns">
+
+    <p class="speech">
+      <strong>Byron</strong> est l'enfant terrible de Captain Beefheart et The Streets, le batard révolté de Tom Waits
+      et The Beastie Boys. Avec un hip-hop cinématographique qui emprunte au blues, au gospel, au folk et au punk, et des textes en anglais incisifs, ils vous entraînent dans les méandres du désespoir amoureux sur un air enjoué de cabaret glauque.
+    </p>
+
+    <p class="speech">
+      Byron c'est : <span class="stabilo">Percy Louis</span>  à la basse,<span class="stabilo"> Erwan Guirriec</span> à la batterie, <span class="stabilo">Antoine Houbron</span> aux claviers, <span class="stabilo">Lewis Cuthbert-Ashton</span> au chant, <span class="stabilo">Yann Boisselier</span> à la guitare et au chant.
+    </p>
+
+  </div>
+</div>
+
+<div class="row">
+  <div class="small-12 columns text-center facebook-wrapper">
+    <p class=""><a target="_blank" href="http://facebook.com/byron.band" class="small secondary button radius"> Voir la page facebook de Byron</a></p>
+    <div class="fb-like" data-href="https://facebook.com/byron.band" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="small-12 columns">
+    <div class="row">
+      <div class="small-12 columns soundcloud-wrapper">
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/199643732&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/199646718&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/foundation/js/foundation.min.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
