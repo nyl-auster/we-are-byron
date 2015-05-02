847a2706bef91a138559ea5162500b8e5ba349d8
yann <yann.boisselier@gmail.com>
2015-05-02 15:00:31 +0200

    up

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
 <div class="row">
   <div class="small-12 columns">
-    <h1 class="text-center" id="byron">BYRON <small>inc</small> </h1>
+    <h1 class="text-center" id="byron">BYRON <small class="hidden-for-small">inc</small> </h1>
     <!--<div class="text-center"><i class="fi-alert"></i> Site temporaire développé conformément à la méthode <a target="_blank" href="http://byatoo.com/la-rache/index.php?p=page&name=la-faq&id=3">la rache</a> </div> -->
   </div>
 </div>
